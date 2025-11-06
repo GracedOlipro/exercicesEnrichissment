ad6c6e50ab9ec2adb92c6ad06ccaf8c0a580f670
Olivier Legault <olivier.legault231@outlook.com>
2025-11-06 10:29:40 -0500

    ex 2 jour 3

--- legaultol/at03/Ex02.java
+++ legaultol/at03/Ex02.java
@@ -1,5 +1,28 @@
 package legaultol.at03;
 
+import java.util.Scanner;
+
 public class Ex02 {
+
+    public static void main(String[] args) {
+        int Tableau[]=new int[10];
+
+        int iCompteur=0,iCompteur2=0;
+        Scanner entScanner =new Scanner(System.in);
+        int inombre;
+
+            for (int i = 0; i < 10; i++){
+                System.out.println("entrez le numéro");
+                inombre=entScanner.nextInt();
+                Tableau[iCompteur]=inombre;
+                iCompteur++;
+            }
+
+          for (int i = 0; i < 10; i++) { System.out.print(Tableau[iCompteur2]);
+          iCompteur2++;}
+
+            
+
+    }
 
 }
