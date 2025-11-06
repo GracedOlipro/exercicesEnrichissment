46cbff0a1609bd8b290fee6f36511facfa0b40ad
Olivier Legault <olivier.legault231@outlook.com>
2025-11-06 10:14:54 -0500

    ex01 jour 3

---
+++ legaultol/at03/Ex01.java
@@ -0,0 +1,22 @@
+package legaultol.at03;
+import java.util.*;
+import java.math.*;
+
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        
+        String sTableau[]= {"allo","bonjour","salut","bon Matin","au revoir"};
+        int iCompteur=0;
+         for (int i = 0; i < 5; i++) { System.out.println(sTableau[iCompteur]);
+          iCompteur++;}
+
+
+       
+
+       
+
+    }
+
+}
---
+++ legaultol/at03/Ex02.java
@@ -0,0 +1,5 @@
+package legaultol.at03;
+
+public class Ex02 {
+
+}
