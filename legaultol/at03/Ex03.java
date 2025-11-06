7ed8bd4af39868394b303be816ca83b4800dc016
Olivier Legault <olivier.legault231@outlook.com>
2025-11-06 12:17:21 -0500

    aaaaaaaaaaaaaaaaaaaaaahh
    la mort

---
+++ legaultol/at03/Ex03.java
@@ -0,0 +1,33 @@
+package legaultol.at03;
+import java.util.*;
+
+public class Ex03 {
+
+    public static void main(String[] args) {
+       /*  int tab[]=new int[5];
+        int iChoix=0,iCompteur=0,iCompteur2=0;
+        Random random = new Random();
+        int iRandomNumber=random.nextInt();
+        Scanner entre1 =new Scanner(System.in);
+        System.out.println("bonjour, entrez le nombre de chiffres aléatoire a afficher");
+        iChoix=entre1.nextInt();
+    
+        
+
+        for (int i=0; i >iChoix; i++)
+            {
+                tab[iCompteur]=iRandomNumber;
+                iCompteur++;
+            }
+        
+
+            for (int i = 0; i < tab.length; i++) { System.out.print(tab[iCompteur2]);
+          iCompteur2++;}*/
+
+    }
+
+
+
+
+
+}
