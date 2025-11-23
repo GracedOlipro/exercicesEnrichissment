c2737f78c2c0e0008b13696eecf24ceba86a9774
Olivier Legault <olivier.legault231@outlook.com>
2025-11-23 15:30:09 -0500

    Create e2qwR.java

---
+++ legaultol/tp01/e2qwR.java
@@ -0,0 +1,13 @@
+package legaultol.tp01;
+
+public class e2qwR {
+
+
+String[] tab_ville={"Le Claire","New York","Paris","Calcutta","Barcelone","Manille","Montreal","Hong Kong"};
+double[] tab_superficie={210,1214,105.4,185,100.4,38.55,363.52,1104};
+int[] tab_pupulation={8452409,8391881,211297,4580544,1621537,1660714,1667700,7018636};
+double[] tab_densite=new double[8];
+
+
+
+}
